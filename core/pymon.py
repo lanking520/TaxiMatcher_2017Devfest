734b9e49288f5d2906edf0745dcc08cfc664ffe1
lanking520 <lanking520@gmail.com>
2017-02-05 11:48:21 -0500

    Test Query

--- core/pymon.py
+++ core/pymon.py
@@ -1,5 +1,4 @@
 from pymongo import MongoClient
-import pprint
 
 try:
 	client = MongoClient('localhost',27017)
@@ -14,3 +13,9 @@ things = taxidb.things
 print things.count()
 
 print things.find_one()
+
+print things.find({'pickup_longitude':{'$lt' : -73.983360, '$gt': -74.0}, 
+	'pickup_latitude':{'$gt' : 40.7609367, '$lt': 40.8009367}, 
+	'tpep_pickup_datetime': {'$gt' : '2016-06-09 21:13:08', '$lt': '2016-06-09 21:23:08'} }).count()
+
+
